e3adb9ab94db83f18a872c76304c28be222023f6
Rahmat Soleh <solehr619@gmail.com>
2022-06-06 09:44:17 +0700

    create status response for register and login

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 /* eslint-disable no-undef */
-const Hapi = require('@hapi/hapi');
-const routes = require('./app/routes.js');
 const dotenv = require('dotenv');
+const Hapi = require('@hapi/hapi');
+const routes = require('./app/routes');
 
 const host = dotenv.config().parsed.APP_HOST || 'localhost';
-const port = process.env.PORT ||  dotenv.config().parsed.APP_PORT || 3000;
+const port = process.env.PORT || dotenv.config().parsed.APP_PORT || 3000;
 
-const init = async() => {
-    const server = Hapi.server({
-        port: port,
-        host: host,
-        routes: {
-            cors: true
-        }
-    });
-    
-    server.route(routes);
-    
-    await server.start();
-    console.log('Server running on %s', server.info.uri);
+const init = async () => {
+  const server = Hapi.server({
+    port,
+    host,
+    routes: {
+      cors: true,
+    },
+  });
+
+  server.route(routes);
+
+  await server.start();
+  console.log('Server running on %s', server.info.uri);
 };
 
 init();
