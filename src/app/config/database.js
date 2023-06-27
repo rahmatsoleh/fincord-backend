dd0e2859e54e235e690eed395e9027fb515dc8f4
rahmat-soleh <solehr619@gmail.com>
2023-06-28 06:32:27 +0700

    Update DB Host

--- src/app/config/database.js
+++ src/app/config/database.js
@@ -1,21 +1,21 @@
 // for configuration
-const mysql = require('mysql2');
-const dotenv = require('dotenv');
+const mysql = require("mysql2");
+const dotenv = require("dotenv");
 
 const connection = mysql.createConnection({
-  host: dotenv.config().parsed.DB_HOST,
-  user: dotenv.config().parsed.DB_USER,
-  password: dotenv.config().parsed.DB_PASSWORD,
-  database: dotenv.config().parsed.DB_DATABASE,
+  host: process.env.DB_HOST || dotenv.config().parsed.DB_HOST,
+  user: process.env.DB_USER || dotenv.config().parsed.DB_USER,
+  password: process.env.DB_PASSWORD || dotenv.config().parsed.DB_PASSWORD,
+  database: process.env.DB_DATABASE || dotenv.config().parsed.DB_DATABASE,
 });
 
 connection.connect((err) => {
   if (err) {
-    console.log('Error connecting to database');
+    console.log("Error connecting to database");
     console.log(err);
     return;
   }
-  console.log('Connection established');
+  console.log("Connection established");
 });
 
 module.exports = connection;
