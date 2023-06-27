daa5e272370c2424654cb8df22a5665548601c96
rahmat-soleh <solehr619@gmail.com>
2023-06-28 06:14:02 +0700

    Update .env

--- knexfile.js
+++ knexfile.js
@@ -1,59 +1,58 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 // Update with your config settings.
 
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 module.exports = {
-
-    development: {
-        client: 'mysql2',
-        connection: {
-            host: dotenv.config().parsed.DB_HOST,
-            user: dotenv.config().parsed.DB_USER,
-            password: dotenv.config().parsed.DB_PASSWORD,
-            database: dotenv.config().parsed.DB_DATABASE
-        },
-        migrations: {
-            directory: './src/app/database/migrations'
-        },
-        seeds: {
-            directory: './src/app/database/seeder'
-        }
+  development: {
+    client: "mysql2",
+    connection: {
+      host:
+        dotenv.config().parsed.DB_HOST || "containers-us-west-133.railway.app",
+      user: dotenv.config().parsed.DB_USER || "root",
+      password: dotenv.config().parsed.DB_PASSWORD || "7cUuXIEibz6roJNm7BNU",
+      database: dotenv.config().parsed.DB_DATABASE || "railway",
     },
-
-    staging: {
-        client: 'mysql2',
-        connection: {
-            host: dotenv.config().parsed.DB_HOST,
-            user: dotenv.config().parsed.DB_USER,
-            password: dotenv.config().parsed.DB_PASSWORD,
-            database: dotenv.config().parsed.DB_DATABASE
-        },
-        pool: {
-            min: 2,
-            max: 10
-        },
-        migrations: {
-            tableName: 'knex_migrations'
-        }
+    migrations: {
+      directory: "./src/app/database/migrations",
     },
+    seeds: {
+      directory: "./src/app/database/seeder",
+    },
+  },
 
-    production: {
-        client: 'mysql2',
-        connection: {
-            host: dotenv.config().parsed.DB_HOST,
-            user: dotenv.config().parsed.DB_USER,
-            password: dotenv.config().parsed.DB_PASSWORD,
-            database: dotenv.config().parsed.DB_DATABASE
-        },
-        pool: {
-            min: 2,
-            max: 10
-        },
-        migrations: {
-            tableName: 'knex_migrations'
-        }
-    }
+  staging: {
+    client: "mysql2",
+    connection: {
+      host: dotenv.config().parsed.DB_HOST,
+      user: dotenv.config().parsed.DB_USER,
+      password: dotenv.config().parsed.DB_PASSWORD,
+      database: dotenv.config().parsed.DB_DATABASE,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
 
+  production: {
+    client: "mysql2",
+    connection: {
+      host: dotenv.config().parsed.DB_HOST,
+      user: dotenv.config().parsed.DB_USER,
+      password: dotenv.config().parsed.DB_PASSWORD,
+      database: dotenv.config().parsed.DB_DATABASE,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
 };
